30d7984abde12e12cec710a09f04dc9c59d2e4ce
Johnathon <ja.lueth@gmail.com>
2014-03-17 18:57:47 -0500

    added klass and semester to main

--- app/main.rb
+++ app/main.rb
@@ -7,5 +7,21 @@ require 'sinatra/reloader'
 require_relative './functions'
 
 class System < Sinatra::Base
+  #student - name, teacher_id, grade_id
+  post "/add_students" do
+    params.each do |new_student|
+       Student.create(:name=>new_student[:name], :teacher_id =>new_student[:teacher_id], :grade_id =>new_student[:grade_id]) 
+     end
+  end
   
+   
+  get "/semester/:id" do
+    @semester=Semester.where(:year=>params[:semester.year], :term=>params[:semester_term])
+    @klasses=Klass.where(:id => semester_id)
+    erb :semester
+  end
+  
+  get "/semester/:year/:spring_or_fall" do
+    @klasses=Klass.where(params[:year]=>:year, params[:spring_or_fall]=>:spring_or_fall)  
+  end
 end
