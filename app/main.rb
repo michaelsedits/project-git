e0cf25eb168d5dad5292a4480e045affc15d8650
Michael Todd <mikejtodd@gmail.com>
2014-03-17 18:50:32 -0500

    Working on add_class.erb and class/add post

--- app/main.rb
+++ app/main.rb
@@ -8,12 +8,29 @@ require_relative './functions'
 
 class System < Sinatra::Base
   get "/" do
-    @past_semesters = Semester.where('current = ?', 'f')
-    
     @klasses = Klass.where('semester_id.current = ?', 't')
+    
+    @past_semesters = Semester.where('current = ?', 'f')
+    erb :home
   end
   
   get "class/add" do
-    
+    erb :add_class
+  end
+  
+  post "class/add"
+  
+  klass = Klass.new({
+    student.each do |student|
+      Student.new(:name => params[:name], :grade => params[:grade])
+    end
+    })
+    student.save
+    :name => params[:name]
+  })
+  klass.save
+  
+  redirect "/"
+  
   end
 end
