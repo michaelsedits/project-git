66a6dd759bb0f3b6a8f729e5912e725486584e18
Michael Todd <mikejtodd@gmail.com>
2014-03-18 09:52:20 -0500

    Edit main.rb

--- app/main.rb
+++ app/main.rb
@@ -26,7 +26,7 @@ class System < Sinatra::Base
   post "/add_class" do
     @student_id = Student.find_by_name(params[:student])
     @teacher_id = Teacher.find_by_name(params[:teacher])
-    @semester_id = Semester.where('year = ?', '#{params[:year]}').where('spring_or_fall = ?', '#{params[:spring_or_fall]}')
+    @semester_id = Semester.where('year = ?', params[:year]}).where('spring_or_fall = ?', params[:spring_or_fall]})
     
     klass = Klass.new({
       :name => params[:name]
